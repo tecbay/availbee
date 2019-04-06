09cc15de99391c1d7deefae8fe92576de4cb5a44
Miraj Khan <miraj.net.xp@gmail.com>
2019-04-06 18:03:25 +0600

    as

--- routes/web.php
+++ routes/web.php
@@ -227,7 +227,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Symfony\Component\Console\Output\NullOutput;
 
-//app()->singleton( WebsocketsLogger::class, function () {
-//	return ( new WebsocketsLogger( new NullOutput() ) )->enable( false );
-//} );
+app()->singleton( WebsocketsLogger::class, function () {
+	return ( new WebsocketsLogger( new NullOutput() ) )->enable( false );
+} );
 WebSocketsRouter::webSocket( '/app/{appKey}', \App\WebSocketHandler::class );
