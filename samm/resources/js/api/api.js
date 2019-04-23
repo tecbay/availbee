887dbb6b22047c0568194451b7a33a5fb068f799
Miraj Khan <miraj.net.xp@gmail.com>
2019-04-24 00:16:22 +0600

    delete semm folder

--- samm/resources/js/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const baseDomain = 'http://localhost/eppbd-lumen/public';
-const baseURL = `${baseDomain}/api/v1/admin`;
-
-export default axios.create({
-    baseURL,
-    // headers:{"Authorization": "Bearar token"}
-});
--- samm/resources/views/includes/footer.blade.php
+++
@@ -1,5 +0,0 @@
-
-    <div class="container-fluid pd-t-0-f ht-100p">
-        <span>&copy; 2019 Availby | All Right Reserved</span>
-        <span>Designed by: Webmaster</span>
-    </div><!-- container -->
